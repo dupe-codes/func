c7a4d7d0deacabc4d11ac48e6f5fad5d49b82443
njdup <njdupp@gmail.com>
2015-03-01 16:07:41 -0800

    Added routes configuration skeleton to server.go

--- server.go
+++ server.go
@@ -8,8 +8,48 @@ package main
 
 import (
     "fmt"
+    "net/http"
+
+    "github.com/gorilla/mux"
+    "github.com/gorilla/sessions"
+    "github.com/gorilla/securecookie"
+
+    //"github.com/njdup/settings"
+    //"github.com/njdup/utils"
+    //"github.com/njdup/users"
+    //"github.com/njdup/commands"
+    //"github.com/njdup/texts"
 )
 
+// TODO: Export this to global settings package
+var (
+    keyLength = 16
+)
+
+// configureRoutes will eventually initialize all application and API routes
+// TODO: Fill this in as components of the application are created
+// All new components must export an InitializeRoutes func, which takes in
+// the app router and session store as params
+func configureRoutes(router *mux.Router, sessionStore *sessions.CookieStore) {
+    // Example:
+    // users.InitializeRoutes(router, sessionStore)
+    router.HandleFunc("/", dummyFunc)
+}
+
+
+func dummyFunc(res http.ResponseWriter, req *http.Request) {
+    fmt.Fprintf(res, "Welcome to Serve!")
+}
+
 func main() {
-    fmt.Printf("Project initialized!\n")
+    router := mux.NewRouter()
+    // TODO: Check the security of this - using the generated random key should
+    // be sufficient, but better safe than sorry
+    sessionStore := sessions.NewCookieStore([]byte(securecookie.GenerateRandomKey(keyLength)))
+    configureRoutes(router, sessionStore)
+    //router.HandleFunc("/", dummyFunc)
+
+    http.Handle("/", router)
+    fmt.Println("Listening on port 8080")
+    http.ListenAndServe(":8080", nil)
 }
